7724e4457fbf30468b70e7fd138e5b6d04ac23b8
A <105343395+Map-Jo@users.noreply.github.com>
2022-08-18 20:40:43 +0900

    제발

--- Stock.py
+++ Stock.py
@@ -17,8 +17,8 @@ st.set_page_config(
 )
 
 with st.sidebar:
-    choose = option_menu("App Gallery", ["About", "Today\'s Korea Stock Market", "Today\'s US Stock Market", "Predict Korea Stocks", "Predict US Stocks"],
-                         icons=['house', 'graph-up-arrow', 'graph-up', 'hurricane','hypnotize'],
+    choose = option_menu("App Gallery", ["About", "Today\'s Korea Stock Market", "Today\'s US Stock Market", "Predict Korea Stocks", "Predict US Stocks", 'Caution'],
+                         icons=['house', 'graph-up-arrow', 'graph-up', 'hurricane','hypnotize', 'exclamation-diamond-fill'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#fafafa"},
@@ -201,8 +201,8 @@ elif choose == "Today\'s US Stock Market":
 
                 st.text(prin +'의 KEB하나은행 환율정보 입니다.')
                 st.text('현재 1$당 '+str(usdletter)+'원 입니다.')
-    elif Name not in Code_name_list:
-        st.text('검색하신 주식 종목이 없습니다. 정확하게 입력해주세요.')
+            elif Name not in Code_name_list:
+                st.text('검색하신 주식 종목이 없습니다. 정확하게 입력해주세요.')
 
 elif choose == "Predict Korea Stocks":
     col1, col2 = st.columns( [0.8,0.2])
@@ -444,3 +444,20 @@ elif choose == "Predict US Stocks":
 
         elif Name not in Code_name_list:
             st.text('검색하신 주식 종목이 없습니다. 정확하게 입력해주세요.')
+elif choose == 'Caution':
+    st.markdown('### 증권투자시 유의사항')
+    st.markdown('* 증권투자는 본인의 판단과 책임으로.')
+    st.text(' - 증권투자는 반드시 자기 자신의 판단과 책임하에 하여야 하며, 자신의 여유자금으로 분산투자하는 것이 좋습니다.')
+    st.text(' - 증권회사 직원 및 타인에게 매매거래를 위임하더라도 투자손익은 고객 자신에게 귀속되며, 투자원금의 보장 또는 손실보전 약속은 법률적으로 효력이 없습니다.')
+    st.markdown('* 높은 수익에는 높은 위험이.')
+    st.text(' - 높은 수익에는 반드시 높은 위험이 따른다는 것을 기억하고 투자시 어떤 위험이 있는지 반드시 확인하시기 바랍니다.')
+    st.text(' - 주식워런트 증권(ELW), 선물·옵션 거래는 단기간에 투자금 전부를 손실 볼 수 있으며 특히 선물·옵션 거래는 투자금을 초과하여 손실 볼 수 있으므로 거래설명서를 교부받고 거래제도와 특성, 거래에 따른 위험 등을 반드시 숙지하여야 합니다.')
+
+    st.markdown('### 간접투자상품 투자시 유의사항')
+    st.markdown('* 원금이나 수익이 보장되지 않습니다.')
+    st.text(' - 수익증권, 일임형 랩(Wrap) 등 간접투자상품은 원금보장이 되지 않으며 운용결과에 따라 투자원금의 손실이 발생할 수 있습니다.')
+    st.text(' - 간접투자상품의 과거 운용실적이 미래의 수익을 보장하는 것은 아닙니다.')
+
+    st.markdown('* 그 밖에 알아두어야 할 사항')
+    st.text(' - 가입한 상품의 기준가격이나 손익현황을 수시로 확인하여 예상되는 시장변동에 대처하여야 합니다.')
+    st.text(' - 해외의 유가증권에 투자하는 간접투자상품은 환율의 변동에 따라 가치가 변동될 수 있습니다.')
