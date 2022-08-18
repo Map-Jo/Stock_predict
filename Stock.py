0457a7da9847bc367e1b5feec10ed253f54d9b1d
A <105343395+Map-Jo@users.noreply.github.com>
2022-08-18 20:54:26 +0900

    caution hyperlink

--- Stock.py
+++ Stock.py
@@ -41,7 +41,7 @@ if choose == "About":
         st.text(' ')
 
     st.markdown('<p class="font">Hello!\n\n저희는 **반포자이까지 한걸음** 입니다.\n\n저희는 *부족한 투자 지식*으로 인한 *투자손실*을 예방하고자 최적의 **포트폴리오**를 제공하고, 내일 예상 **주가를 예측**할 수 있는 사이트입니다.\n\n많이 부족하지만 **재미로만** 봐주시기를 부탁드립니다.</p>', unsafe_allow_html=True)
-    st.markdown('[유의사항](https://map-jo-stock-predict-stock-73rqcb.streamlitapp.com/Caution)')
+    st.markdown('[유의사항](https://map-jo-stock-predict-stock-73rqcb.streamlitapp.com/#https://map-jo-stock-predict-stock-73rqcb.streamlitapp.com/Caution)')
     image = Image.open('data/stockcode.jpg')
     st.image(image, width=800, caption= 'The Great GATSBY')
 
