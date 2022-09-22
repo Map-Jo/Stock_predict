77d93186a76abff2de241c6874181ec1cb96609c
Map-Jo <ahyun1609@likelion.org>
2022-09-22 23:19:25 +0900

    Update Stock.py

--- Stock.py
+++ Stock.py
@@ -88,7 +88,7 @@ elif choose == "Today\'s Korea Stock Market":
             col1, col2, col3 = st.columns(3)
             col1.metric("현재 주식가격",format(df['Close'].tail(1)[0], ',')+'원', "%d원" %(df['Close'].diff().tail(1)[0]))
             col2.metric("현재 거래량", format(df['Volume'].tail(1)[0], ','),"%.2f%%" %(df['Volume'].pct_change().tail(1)[0] * 100))
-            col3.metric("전일 대비 가격", "%d원" %(df['Close'].diff().tail(1)[0]), "%.2f%%" %(df['Change'].tail(1)[0] * 100))
+            col3.metric("전일 대비 가격", "%d원" %(df['Close'].diff().tail(1)[0]), "%.2f%%" %(df['Close'].pct_change().tail(1)[0] * 100))
 
             fig = px.line(df, y='Close', title='{} 종가 Time Series'.format(Name))
 
@@ -154,7 +154,7 @@ elif choose == "Predict Korea Stocks":
     if Name in Code_name_list:
         code_num = Stockcode.at[Name, 'code']
         data = pdr.get_data_yahoo(code_num)
-        data['Change'] = data['Close'].diff()
+        data['Change'] = data['Close'].pct_change()
         with st.spinner('Predicting...'):
             if data.shape[0] >= 60:
                 startdate = (dt.datetime.now()-dt.timedelta(days=31)).strftime('%Y-%m-%d')
